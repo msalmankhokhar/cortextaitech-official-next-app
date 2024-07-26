6b9ad6c722732658645da105e7d0aee0bc55bd6a
msalmankhokhar <kjokhars@gmail.com>
2024-07-26 22:38:51 +0500

    changed number badges design in procedure section

--- components/ProcedureBox.js
+++ components/ProcedureBox.js
@@ -14,8 +14,10 @@ export default function ProcedureBox({title, content, number}) {
 
     return (
         <div data-aos="fade-up" className="flex flex-col items-center w-full bg-black rounded-lg gap-3 ring-2 ring-amber-500 p-10 relative max-w-sm">
-            <div className="number-badge rounded-full bg-amber-500 absolute flex items-center justify-center">
-                <span className="leading-none font-bold text-lg text-white">{number}</span>
+            <div className='number-badge absolute rounded-full'>
+                <div className="wrapper rounded-full bg-amber-500 flex items-center justify-center">
+                    <span className="leading-none font-bold text-lg text-white">{number}</span>
+                </div>
             </div>
             <h1 className="text-sm font-bold leading-none text-white">{title}</h1>
             <p className="text-xs text-gray-200 text-justify">
